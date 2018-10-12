d2d68ae24c705774b54a0c62354d99dcafcac997
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-12 14:26:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,11 +8,12 @@ def batch_badge_creator(array)
   badges_messages=[]
  
   array.each { |name| badges_messages << "Hello, my name is #{name}."}
-    
   badges_messages
-    
 end 
 
 def assign_rooms(list_of_speakers)
+  
+  
+  
   
 end 
